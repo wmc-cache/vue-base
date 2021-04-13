f8313ea0715d7d1695269031a61d9bee4a300fb0
wmc <wu__mengcheng@163.com>
2021-04-14 00:43:50 +0800

    wmc

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import router from "./routes/index"
 import App from './App.vue'
 import store from "./store/index"
+import "normalize.css/normalize.css"
+
 const app = createApp(App)//根据APP组件创建单页应用
 
 app.use(router).use(store)
