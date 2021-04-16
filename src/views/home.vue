0b7df930bf97b64ee3bce2ff35d13780435a4102
wmc-cache <1006004868@qq.com>
2021-04-16 16:52:33 +0800

    wmc

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
-    <div v-for="component in components" :key="component.id">
-      {{ component.props.text }}
-    </div>
-    <div class="css">css</div>
-    <validate-from>
-      <validate-input
-        :rules="inputRules"
-        v-model:value="inputValueRef"
-      ></validate-input>
-      {{ inputValueRef }}
-    </validate-from>
-  </div>
+	<div>
+		<div
+			v-for="component in components"
+			:key="component.id"
+		>
+			{{ component.props.text }}
+		</div>
+		<div class="css">css</div>
+		<validate-from>
+			<validate-input
+				:rules="inputRules"
+				v-model:value="inputValueRef"
+			></validate-input>
+			{{ inputValueRef }}
+		</validate-from>
+		<a href="">123</a>
+	</div>
 </template>
 
 <script lang="ts">
@@ -22,38 +26,38 @@ import { useStore } from "vuex";
 import { GlobalDataProps } from "../store/index";
 
 export default defineComponent({
-  name: "Home",
-  components: {
-    ValidateInput,
-    ValidateFrom,
-  },
-  setup() {
-    const store = useStore<GlobalDataProps>();
-    const components = computed(() => {
-      return store.state.editor.components;
-    });
-    console.log(components.value);
-    const inputValueRef = ref("wmc");
-    const inputRules: RulesProp = [
-      {
-        type: "required",
-        message: "用户名不能为空",
-      },
-    ];
-    return {
-      inputValueRef,
-      inputRules,
-      components,
-    };
-  },
+	name: "Home",
+	components: {
+		ValidateInput,
+		ValidateFrom,
+	},
+	setup() {
+		const store = useStore<GlobalDataProps>();
+		const components = computed(() => {
+			return store.state.editor.components;
+		});
+		console.log(components.value);
+		const inputValueRef = ref("wmc");
+		const inputRules: RulesProp = [
+			{
+				type: "required",
+				message: "用户名不能为空",
+			},
+		];
+		return {
+			inputValueRef,
+			inputRules,
+			components,
+		};
+	},
 });
 </script>
 
 <style lang="scss">
 .css {
-  width: 200px;
-  height: 200px;
-  background-color: aquamarine;
-  @include box-center;
+	width: 200px;
+	height: 200px;
+	background-color: aquamarine;
+	@include box-center;
 }
 </style>
