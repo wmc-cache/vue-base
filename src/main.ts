df250d404118bf0bfd5dad88c00a55eb1a8fb1f3
wmc <wu__mengcheng@163.com>
2021-04-28 00:50:52 +0800

    wmc

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import "normalize.css/normalize.css"
 import "./style/index.scss"
 
 const app = createApp(App)//根据APP组件创建单页应用
-
+console.log(process.env)
 app.use(router).use(store)
 app.mount('#app')
 
