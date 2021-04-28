c66f4158013e7890f98fe6b60d9f677f07c31df0
wmc-cache <1006004868@qq.com>
2021-04-28 09:44:19 +0800

    wmc

--- src/views/editor.vue
+++ src/views/editor.vue
@@ -1,29 +1,33 @@
 <template>
-  <div class="content">
-    <div>
-      <components-list
-        @onItemClick="addItem"
-        :list="defaultTextTemplates"
-      ></components-list>
-    </div>
-    <div class="middle">
-      <edit-wrapper
-        @setActive="setActive"
-        v-for="component in components"
-        :key="component.id"
-        :id="component.id"
-        :active="component.id === (currentElement && currentElement.id)"
-      >
-        <component :is="component.name" v-bind="component.props" />
-      </edit-wrapper>
-    </div>
+	<div class="content">
+		<div>
+			<components-list
+				@onItemClick="addItem"
+				:list="defaultTextTemplates"
+			></components-list>
+		</div>
+		<div class="middle">
+			<edit-wrapper
+				@setActive="setActive"
+				v-for="component in components"
+				:key="component.id"
+				:id="component.id"
+				:active="component.id === (currentElement && currentElement.id)"
+			>
+				<component
+					:is="component.name"
+					v-bind="component.props"
+				/>
+			</edit-wrapper>
+		</div>
 
-    <div class="right">
-      <pre>
+		<div class="right">
+
+			<pre>
         {{ currentElement && currentElement.props }}
       </pre>
-    </div>
-  </div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
@@ -36,45 +40,45 @@ import { defaultTextTemplates } from "../../defaultTemplates";
 import { ComponentData } from "@/store/editor";
 import EditWrapper from "../components/EditWrapper.vue";
 export default defineComponent({
-  name: "Editor",
-  components: {
-    WText,
-    ComponentsList,
-    EditWrapper,
-  },
-  setup() {
-    const store = useStore<GlobalDataProps>();
-    const components = computed(() => {
-      return store.state.editor.components;
-    });
-    const currentElement = computed<ComponentData | null>(
-      () => store.getters.getCurrentElement
-    );
-    const addItem = (component: any) => {
-      store.commit("addComponent", component);
-    };
-    const setActive = (id: string) => {
-      store.commit("setActive", id);
-    };
+	name: "Editor",
+	components: {
+		WText,
+		ComponentsList,
+		EditWrapper,
+	},
+	setup() {
+		const store = useStore<GlobalDataProps>();
+		const components = computed(() => {
+			return store.state.editor.components;
+		});
+		const currentElement = computed<ComponentData | null>(
+			() => store.getters.getCurrentElement
+		);
+		const addItem = (component: any) => {
+			store.commit("addComponent", component);
+		};
+		const setActive = (id: string) => {
+			store.commit("setActive", id);
+		};
 
-    return {
-      components,
-      defaultTextTemplates,
-      currentElement,
-      addItem,
-      setActive,
-    };
-  },
+		return {
+			components,
+			defaultTextTemplates,
+			currentElement,
+			addItem,
+			setActive,
+		};
+	},
 });
 </script>
 
 <style lang="scss" scoped>
 .content {
-  display: flex;
+	display: flex;
 }
 .middle {
-  display: flex;
-  flex-direction: column;
-  margin-left: 100px;
+	display: flex;
+	flex-direction: column;
+	margin-left: 100px;
 }
 </style>
