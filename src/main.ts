ddff17b57b654ecab0fee63689f5897130cdbcdb
wmc <wu__mengcheng@163.com>
2021-05-07 20:43:33 +0800

    wmc

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,13 @@ import store from "./store/index"
 import "normalize.css/normalize.css"
 import "./style/index.scss"
 
+import legoBricks from "lego-bricks"
+
+import "lego-bricks/dist/bundle.css"
+
 const app = createApp(App)//根据APP组件创建单页应用
 console.log(process.env)
-app.use(router).use(store)
+app.use(router).use(store).use(legoBricks)
 app.mount('#app')
 
 
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,28 +1,28 @@
 <template>
-	<div>
-		<div>
-			{{position}}
-		</div>
-		<input type="color">
-		<message message="吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成"></message>
-		<div
-			v-for="component in components"
-			:key="component.id"
-		>
-			{{ component.props.text }}
-		</div>
-		<div class="css">css</div>
+  <div>
+    <div>
+      {{ position }}
+    </div>
+    <l-text text="wmc"></l-text>
+    <input type="color" />
+    <message
+      message="吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成吴梦成"
+    ></message>
+    <div v-for="component in components" :key="component.id">
+      {{ component.props.text }}
+    </div>
+    <div class="css">css</div>
 
-		<validate-from>
-			<validate-input
-				:rules="inputRules"
-				v-model:value="inputValueRef"
-			></validate-input>
-			{{ inputValueRef }}
-		</validate-from>
-		<a href="">123</a>
-		<input type="text">
-	</div>
+    <validate-from>
+      <validate-input
+        :rules="inputRules"
+        v-model:value="inputValueRef"
+      ></validate-input>
+      {{ inputValueRef }}
+    </validate-from>
+    <a href="">123</a>
+    <input type="text" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -35,41 +35,41 @@ import { GlobalDataProps } from "../store/index";
 import useMousePosition from "../hooks/useMousePosition";
 
 export default defineComponent({
-	name: "Home",
-	components: {
-		ValidateInput,
-		ValidateFrom,
-		Message,
-	},
-	setup() {
-		const position = useMousePosition();
-		const store = useStore<GlobalDataProps>();
-		const components = computed(() => {
-			return store.state.editor.components;
-		});
-		console.log(components.value);
-		const inputValueRef = ref("wmc");
-		const inputRules: RulesProp = [
-			{
-				type: "required",
-				message: "用户名不能为空",
-			},
-		];
-		return {
-			inputValueRef,
-			inputRules,
-			components,
-			position,
-		};
-	},
+  name: "Home",
+  components: {
+    ValidateInput,
+    ValidateFrom,
+    Message,
+  },
+  setup() {
+    const position = useMousePosition();
+    const store = useStore<GlobalDataProps>();
+    const components = computed(() => {
+      return store.state.editor.components;
+    });
+    console.log(components.value);
+    const inputValueRef = ref("wmc");
+    const inputRules: RulesProp = [
+      {
+        type: "required",
+        message: "用户名不能为空",
+      },
+    ];
+    return {
+      inputValueRef,
+      inputRules,
+      components,
+      position,
+    };
+  },
 });
 </script>
 
 <style lang="scss">
 .css {
-	width: 200px;
-	height: 200px;
-	background-color: $color-primary;
-	@include box-center;
+  width: 200px;
+  height: 200px;
+  background-color: $color-primary;
+  @include box-center;
 }
 </style>
