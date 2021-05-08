718d82d655df2439869191827e0abb1016844684
wmc-cache <1006004868@qq.com>
2021-05-08 14:47:04 +0800

    wmc

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import store from "./store/index"
 import "normalize.css/normalize.css"
 import "./style/index.scss"
 
-import legoBricks from "lego-bricks"
+import wmcComponents from "wmc-components"
 
-import "lego-bricks/dist/bundle.css"
+import "wmc-components/dist/bundle.css"
 
 const app = createApp(App)//根据APP组件创建单页应用
 console.log(process.env)
-app.use(router).use(store).use(legoBricks)
+app.use(router).use(store).use(wmcComponents)
 app.mount('#app')
 
 
